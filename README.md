6e6589f508b1be6070c5d694c34a706a28419d3d
JoeMoncrieff <joseph.moncrieff@gmail.com>
2024-07-26 10:49:14 +0100

    B: added report functionality + README changes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # bookbot
+## Functionality
+Bookbot is a bot that can, given a path to a txt file, give you the word and character count of that file.
 
-BookBot is my first project!
+### sidenote
+Bookbot isn't really a project so much as an introduction into a proper workflow and work setup this includes VScode, git, and command line set up
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# Returns the total amount of words in a given string
 def count_words(book):
     #Inefficient soln? (space)
     return(len(book.split(" ")))
 
+#Takes a string and returns a dict of characters and how many times they appeared
 def count_characters(book):
     #Data cleaning
     book = book.lower()
@@ -11,15 +13,47 @@ def count_characters(book):
         output_dict[c] = output_dict.get(c,0) + 1
     return output_dict
 
-
+#Opens a book from a specified path and returns it as a string
 def open_book(path):
     with open(path) as f:
         file_contents = f.read()
         return file_contents
 
+#Dictionary sorter helper
+def s_d_h(dictionary):
+    return dictionary["qty"]
+
+#Sorts a dictionary and returns a list in descending order
+def sort_dictionary(input_dict):
+    dict_list = []
+    for key in input_dict:
+        dict_list.append({"name":key, "qty":input_dict[key]})
+    
+    dict_list.sort(reverse=True, key=s_d_h)
+    return dict_list
+
+#Returns a formatted report analysing a given book/string
+def get_report(path,book):
+    return_string = ""
+    dictionary = count_characters(book)
+    num_words = count_words(book)
+    dict_list = sort_dictionary(dictionary)
+    
+    return_string += f"--- Begin report of {path} ---\n"
+    return_string += f"{num_words} words found in the document\n\n\n"
+    for item in dict_list:
+        if item["name"].isalpha():
+            return_string += f"The \'{item['name']}\' character was found {item['qty']} times\n"
+    return_string += "\n--- End Report ---"
+    return return_string
+
+
+
 def main():
-    book = open_book("books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+    book = open_book(path)
     print(count_words(book))
     print(count_characters(book))
+    print(get_report(path, book))
     
 main()
